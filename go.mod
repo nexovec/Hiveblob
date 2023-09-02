2041314adfea55f0c94f59f109bd11a9728b59c2
nexovec <hraccesky@gmail.com>
2023-09-02 18:40:07 +0200

    rendering a box2d body outline

--- go.mod
+++ go.mod
@@ -3,22 +3,23 @@ module hiveblob
 go 1.20
 
 require (
+	github.com/E4/box2d v1.0.2
 	github.com/dusk125/pixelui v0.1.0
-	github.com/faiface/beep v1.1.0
+	github.com/dusk125/pixelutils v1.0.0
 	github.com/faiface/pixel v0.11.0-beta
+	github.com/inkyblackness/imgui-go v1.12.0
 	github.com/spf13/cobra v1.7.0
 	golang.org/x/image v0.0.0-20210607152325-775e3b0c77b9
 )
 
 require (
-	github.com/dusk125/pixelutils v1.0.0 // indirect
+	github.com/ByteArena/box2d v1.0.2 // indirect
 	github.com/faiface/glhf v0.0.0-20181018222622-82a6317ac380 // indirect
 	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
 	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20191125211704-12ad95a8df72 // indirect
 	github.com/go-gl/mathgl v0.0.0-20190416160123-c4601bc793c7 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/inkyblackness/imgui-go v1.12.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pzsz/voronoi v0.0.0-20130609164533-4314be88c79f // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
-	pixelui "github.com/dusk125/pixelui"
-	"github.com/inkyblackness/imgui-go"
 	"log"
 	"os"
+
+	pixelui "github.com/dusk125/pixelui"
+	"github.com/inkyblackness/imgui-go"
+	"golang.org/x/image/colornames"
+
 	// beep "github.com/faiface/beep"
+	box2d "github.com/E4/box2d"
 	pixelutils "github.com/dusk125/pixelutils"
 	pixel "github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	pixelgl "github.com/faiface/pixel/pixelgl"
-	colornames "golang.org/x/image/colornames"
 
 	cobra "github.com/spf13/cobra"
 )
@@ -19,6 +23,9 @@ var VERSION = "v0.0.0-dev"
 var WINDOW_TITLE = "Hiveblob"
 var WINDOW_HEIGHT float64 = 1080
 var WINDOW_ASPECT_RATIO float64 = 16.0 / 9.0
+
+var VELOCITY_ITERATIONS int = 6
+var POSITION_ITERATIONS int = 2
 
 type LaunchOptions struct {
 	verbose        bool
@@ -46,17 +53,99 @@ func run() {
 	defer ui.Destroy()
 
 	// font := imgui.CurrentIO().Fonts().AddFontDefault()
-	// ui.AddTTFFont("resources/fonts/bitstream_vera_mono/VeraMono.ttf", 48)
+	ui.AddTTFFont("resources/fonts/bitstream_vera_mono/VeraMono.ttf", 48)
 	// font := imgui.CurrentIO().Fonts().AddFontFromFileTTF("resources/fonts/bitstream_vera_mono/VeraMono.ttf", 12)
+	// fmt.Printf("imgui.CurrentIO().Fonts(): %v\n", imgui.CurrentIO().Fonts())
 	// ui.AddTTFFont()
-	// imgui.PushFont(font)
+	// imgui.PushFont(ui.fonts.)
 
 	ticker := pixelutils.NewTicker(60)
+
+	// set-up physics
+	gravityVector := box2d.B2Vec2{
+		X: 0.0,
+		Y: 0.0}
+	world := box2d.MakeB2World(gravityVector)
+
+	groundBodyDef := box2d.MakeB2BodyDef()
+	groundBodyDef.Position.Set(20.0, 10.0)
+
+	groundShape := box2d.MakeB2PolygonShape()
+	groundShape.SetAsBox(15.0, 3.0)
+
+	groundFixture := box2d.MakeB2Fixture()
+	groundFixture.SetDensity(0.0)
+	groundFixture.M_shape = &groundShape
+
+	groundBody := world.CreateBody(&groundBodyDef)
+	groundBody.CreateFixture(&groundShape, 0.0)
+
+	playerBodyDef := box2d.MakeB2BodyDef()
+	// playerBodyDef.Type = box2d.B2BodyType.B2_dynamicBody
+	playerBodyDef.Position.Set(23.0, 4.0)
+
+	playerShape := box2d.MakeB2PolygonShape()
+	playerShape.SetAsBox(1.0, 2.0)
+
+	playerFixtureDef := box2d.MakeB2FixtureDef()
+	playerFixtureDef.Density = 1.0
+	playerFixtureDef.Friction = 0.3
+	playerFixtureDef.Shape = &playerShape
+
+	playerBody := world.CreateBody(&playerBodyDef)
+	playerBody.CreateFixtureFromDef(&playerFixtureDef)
+
+	// gameObjects := struct
 	for !window.Closed() {
+
+		// game update
+		dt := ticker.Deltat()
+
+		world.Step(dt, VELOCITY_ITERATIONS, POSITION_ITERATIONS)
+		// game rendering
 		window.Clear(colornames.Beige)
-		ui.NewFrame()
+		// DEBUG: render a circle out of on the screen.
+		// {
+		// 	ctx := imdraw.New(nil)
+		// 	ctx.Color = colornames.Black
+		// 	ctx.Push(pixel.V(100.0, 0.0))
+		// 	ctx.Circle(100.0, 100.0)
+		// 	ctx.Draw(window)
+		// }
+		DRAW_PHYSICS_OBJ_OUTLINES := true
+		if DRAW_PHYSICS_OBJ_OUTLINES {
+			DEBUG_fixture := groundBody.GetFixtureList()
+			if DEBUG_fixture.GetType() == box2d.B2Shape_Type.E_polygon {
+				shape := DEBUG_fixture.GetShape()
+				shape.Clone()
+				polygon := shape.(*box2d.B2PolygonShape)
+				vertices := polygon.M_vertices
+				for i := 0; i < polygon.M_count; i++ {
+					vertex := vertices[i]
+					nextVertex := vertices[(i+1)%polygon.M_count]
+					position := groundBody.GetWorldPoint(vertex)
+					nextPosition := groundBody.GetWorldPoint(nextVertex)
+					BOX2D_RENDERING_SCALE := 10.0
+					p1 := pixel.V(position.X, position.Y).Scaled(BOX2D_RENDERING_SCALE)
+					p2 := pixel.V(nextPosition.X, nextPosition.Y).Scaled(BOX2D_RENDERING_SCALE)
+					// line := pixel.Line{
+					// 	p1,
+					// 	p2,
+					// }
+					ctx := imdraw.New(nil)
+					ctx.Color = colornames.Black
+					fmt.Println(p1, p2)
+					ctx.Push(p1, p2)
+					ctx.Line(5.0)
+					ctx.Draw(window)
+				}
+			} else {
+				panic("Not implemented!")
+			}
+		}
 
 		// UI goes here
+		ui.NewFrame()
 		imgui.Begin("Base")
 		// imgui.PushFont(font)
 		imgui.Text("Placeholder text")
