c8d9742b7070ef646b5262897a6df5f2a0f47d72
nexovec <hraccesky@gmail.com>
2023-09-16 23:38:12 +0200

    added resources

---
+++ resources/dyno1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="dyno1" tilewidth="24" tileheight="24" tilecount="24" columns="24">
+ <image source="resources/tilesets/DinoSprites - doux.png" width="576" height="24"/>
+</tileset>
